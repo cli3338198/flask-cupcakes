68b1cc371716dcc972bae7116fbe3f61c5f8ee9f
Christopher Lim <cli3338198@gmail.com>
2022-10-24 12:37:53 -0700

    finished routes

--- back/app.py
+++ back/app.py
@@ -1,1 +1,47 @@
-"""Flask app for Cupcakes"""
+from flask import Flask, url_for, render_template, redirect, flash, jsonify, request
+
+# from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "secret"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+connect_db(app)
+db.create_all()
+
+@app.route('/api/cupcakes', methods=["GET"])
+def cupcake_all():
+  """Get all cupcakes."""
+
+  cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+
+  return jsonify(cupcakes=cupcakes)
+
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=["GET"])
+def cupcake_get(cupcake_id):
+  """Get a cupcake."""
+
+  cupcake = Cupcake.query.get_or_404(cupcake_id).serialize()
+
+  return jsonify(cupcake=cupcake)
+
+@app.route('/api/cupcakes', methods=["POST"])
+def cupcake_post():
+  """Add a cupcake."""
+
+  flavor = request.json['flavor']
+  size = request.json['size']
+  rating = request.json['rating']
+  image = request.json['image']
+
+  cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image=image)
+
+  db.session.add(cupcake)
+  db.session.commit()
+
+  return (jsonify(cupcake=cupcake.serialize()), 201)
--- back/models.py
+++ back/models.py
@@ -1,1 +1,63 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Cupcake(db.Model):
+  """Cupcake."""
+
+  __tablename__ = "cupcakes"
+
+  id = db.Column(
+    db.Integer,
+    primary_key=True,
+    autoincrement=True
+  )
+
+  size = db.Column(
+    db.String(50),
+    nullable=False,
+  )
+
+  flavor = db.Column(
+    db.String(50),
+    nullable=False,
+    unique=True
+  )
+
+  rating = db.Column(
+    db.Integer,
+    nullable=False
+  )
+
+  image = db.Column(
+    db.String(1000),
+    nullable=False,
+    default='https://tinyurl.com/demo-cupcake'
+  )
+
+  def __repr__(self):
+    """Cupcake"""
+
+    return f"""<Cupcake {self.id} {self.size} {self.flavor} {self.rating}>"""
+
+  def serialize(self):
+
+    return {
+      'id': self.id,
+      'flavor': self.flavor,
+      'rating': self.rating,
+      'size': self.size,
+      'image': self.image
+    }
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
